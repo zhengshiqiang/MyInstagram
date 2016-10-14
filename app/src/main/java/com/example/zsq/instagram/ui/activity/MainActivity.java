2231cbdc0b9939c4870ea13aa5913ed4511aafb8
zhengshiqiang <zsq@dding.net>
2016-10-14 16:48:21 +0800

    add SendingProgressView

--- app/src/main/java/com/example/zsq/instagram/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/zsq/instagram/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.zsq.instagram;
+package com.example.zsq.instagram.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.zsq.instagram.R;
 
 public class MainActivity extends AppCompatActivity {
 
